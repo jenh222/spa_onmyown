69f6705bc9a79ab0c17c5dc2979a8446d1b8bb79
Jennifer Han <jenniferhan@Jennifers-MBP.attlocal.net>
2020-02-19 02:35:54 -0800

    Ajax Contact Form

--- src/App/Pages/Contact/ContactForm/ContactForm.jsx
+++ src/App/Pages/Contact/ContactForm/ContactForm.jsx
@@ -1,19 +1,62 @@
 import React, { useRef, useState } from 'react';
 import './ContactForm.scss';
 import { emailCheck } from '../../../common/utilities.js';
+import classnames from 'classnames';
+import keycode from 'keycode';
+import API from '../../../common/API.js';
 
 const ContactForm = () => {
 
     //Keep track of state
     const [emailIsValid, updateEmailIsValid] = useState(true);
+    const [formIsValid, updateFormIsValid] = useState(true);
+    const [errors, updateErrorsArray] = useState([]);
 
     // refs :: Giving React's Virtual DOM access to the Physical DOM element
     const emailRef = useRef();
     const messageRef = useRef();
 
 
-    const handleClick = () => {
+    const handleFormSubmit = () => {
         console.log('you clicked');
+
+        let errorMessages = [];
+
+        // Validate the user filled in the form
+        if (emailRef.current.value.length < 4) {
+            errorMessages.push({
+                message: 'You forgot to fill out the Email field',
+            })
+        }
+        if (!emailCheck(emailRef.current.value)) {
+            errorMessages.push({
+                message: 'The email you provided is invalid',
+            })
+        }
+        if (messageRef.current.value.length < 1) {
+            errorMessages.push({
+                message: 'You forgot to fill out the Message field',
+            })
+        }
+
+        updateErrorsArray(errorMessages);
+
+        // Keep track of errors - and update the DOM with feedback if there is an error
+        if (errorMessages.length > 0) {
+            updateFormIsValid(false);
+        }else {
+            updateFormIsValid(true);
+            // If all is successful - we want to post the data
+            
+            const postData = {
+                email: emailRef.current.value,
+                message: messageRef.current.value,
+            }
+
+            API.post('sendmail', postData).then((result) => {
+                console.log('Posting data', result);
+            });
+        }
     }
 
     const validateEmail = () => {
@@ -27,8 +70,47 @@ const ContactForm = () => {
         }
     }
 
+    const displayErrors = () => {
+        return errors.map((error, idx) => {
+            return (
+                <li key={ idx }>{error.message}</li>
+            );
+        });
+    }
+
+        //Handle keyboard events
+    const handleKeyDown = (event) => {
+        switch (keycode(event)) {
+            case 'space':
+            case 'enter':
+                handleFormSubmit();
+                break;
+            default:
+                return true;
+        }
+    }
+
+    //const theClassname = (formIsValid) ? 'ContactForm form-valid':'ContactForm form-invalid';
+
+    const theClassname = classnames({
+        'ContactForm': true,
+        'form-valid': formIsValid,
+        'form-invalid': !formIsValid,
+    });
+
     return (
-        <div className={ 'ContactForm' }>
+        <div className={ theClassname }>
+
+            {
+                (errors.length > 0) &&
+                <div className = "error-message">
+                    ERROR MESSAGE HERE
+                    <ul>
+                        {displayErrors()}
+                    </ul>
+                </div>
+            }
+
             <div className="form-group">
                 <div className="left">
                     <label htmlFor="Email">Email</label>
@@ -56,8 +138,10 @@ const ContactForm = () => {
                 <div className="left" />
                 <div className="right">
                     <button
-                        onClick={ handleClick }
-                    >SEND</button>
+                        tab-index={0}
+                        onClick={ handleFormSubmit }
+                        onKeyDown={ handleKeyDown }
+                    >SEND EMAIL</button>
                 </div>
         </div>
         </div>
