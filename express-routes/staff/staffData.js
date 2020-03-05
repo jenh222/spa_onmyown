a83bf71356991b37e978013d348bd38f963a4443
Jennifer Han <jenniferhan@Jennifers-MacBook-Pro.local>
2020-03-04 20:22:21 -0800

    updated staff images folder to lowercase

--- express-routes/staff/staffData.js
+++ express-routes/staff/staffData.js
@@ -1,34 +1,34 @@
 module.exports = [
     {
-        image: '/img/Staff/staff-1.jpeg',
+        image: '/img/staff/staff-1.jpeg',
         name: 'George',
     },
     {
-        image: '/img/Staff/staff-2.jpeg',
+        image: '/img/staff/staff-2.jpeg',
         name: 'Sherry',
     },
     {
-        image: '/img/Staff/staff-3.jpeg',
+        image: '/img/staff/staff-3.jpeg',
         name: 'Laura',
     },
     {
-        image: '/img/Staff/staff-4.jpeg',
+        image: '/img/staff/staff-4.jpeg',
         name: 'Jan',
     },
     {
-        image: '/img/Staff/staff-5.jpeg',
+        image: '/img/staff/staff-5.jpeg',
         name: 'Susan',
     },
     {
-        image: '/img/Staff/staff-6.jpeg',
+        image: '/img/staff/staff-6.jpeg',
         name: 'Claire',
     },
     {
-        image: '/img/Staff/staff-7.jpeg',
+        image: '/img/staff/staff-7.jpeg',
         name: 'Steph',
     },
     {
-        image: '/img/Staff/staff-8.jpeg',
+        image: '/img/staff/staff-8.jpeg',
         name: 'Tracy',
     },
 ];
